ee3306739af3e263aa10a60d6d02350a18c0767b
zhangqi <zhangqi@able-elec.com>
2019-07-01 16:45:17 +0800

    头部样式完成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
 import { RouterModule,Routes } from '@angular/router';
+
+// ng-zorro-antd
+import { NzModalModule } from 'ng-zorro-antd/modal';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './component/main/main.component';
@@ -10,7 +15,10 @@ import { HeaderComponent } from './component/header/header.component';
 import { AudioPlayingComponent } from './component/audio-playing/audio-playing.component';
 import { SideBarComponent } from './component/side-bar/side-bar.component';
 
-
+/** 配置 angular i18n **/
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+registerLocaleData(zh);
 
 const appRoutes:Routes = [
   {
@@ -29,10 +37,18 @@ const appRoutes:Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot(appRoutes),
+
+    /** 导入 ng-zorro-antd 模块 **/
+    NgZorroAntdModule,
+    NzModalModule,
   ],
-  providers: [],
+  providers: [
+    /** 配置 ng-zorro-antd 国际化（文案 及 日期） **/
+    { provide: NZ_I18N, useValue: zh_CN }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
